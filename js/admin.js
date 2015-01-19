6893afdf153af30db6631e5df165b17b0aa3a91c
Patrick Ward <patrick@standardspace.com>
2015-01-19 16:28:16 +0000

    Parse test

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("ARMASIZaod9C5DVs8XHUtiBujUgCyORXgWC4wSji", "W1GrZcNSe5vVciFkKZu8Xo6RQpJ7S0iuacjP6SeB");
+
+ 
+});
