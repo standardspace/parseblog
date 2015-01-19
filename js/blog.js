43ffc911b37d0e37dcf7e011e2482b293781cb8c
Patrick Ward <patrick@standardspace.com>
2015-01-19 16:08:20 +0000

    Parse test

--- js/blog.js
+++ js/blog.js
@@ -14,12 +14,24 @@ $(function() {
 	var blogs = new Blogs();
 
 	blogs.fetch({
-	    success: function(blogs) {
-	        console.log(blogs);
-	    },
+		success: function(blogs) {
+		    var blogsView = new BlogsView({ collection: blogs });
+		    blogsView.render();
+		    $('.main-container').html(blogsView.el);
+		}
 	    error: function(blogs, error) {
 	        console.log(error);
 	    }
 	});
+
+	var BlogsView =  Parse.View.extend({
+	    template: Handlebars.compile($('#blogs-tpl').html()),
+	    render: function(){ 
+	        var collection = { blog: this.collection.toJSON() };
+	        this.$el.html(this.template(collection));
+	    }
+	});
+
+	
  
 });
