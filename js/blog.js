26e3fac0935ef391d294ff056c7989791b514497
Patrick Ward <patrick@standardspace.com>
2015-01-19 15:49:34 +0000

    Parse retrive blog posts

--- js/blog.js
+++ js/blog.js
@@ -4,11 +4,22 @@ $(function() {
  
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("ARMASIZaod9C5DVs8XHUtiBujUgCyORXgWC4wSji", "W1GrZcNSe5vVciFkKZu8Xo6RQpJ7S0iuacjP6SeB");
- 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
+
+    var Blog = Parse.Object.extend("Blog");
+
+    var Blogs = Parse.Collection.extend({
+	    model: Blog
+	});
+
+	var blogs = new Blogs();
+
+	blogs.fetch({
+	    success: function(blogs) {
+	        console.log(blogs);
+	    },
+	    error: function(blogs, error) {
+	        console.log(error);
+	    }
+	});
  
 });
